609ca0cadfbe97e7d672d7244127e1ccdd75867d
Scott Simon <ssimon@esri.com>
2021-05-25 10:41:45 -0700

    Cleanup

--- app/src/main/java/com/example/acmeonlinestore/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/acmeonlinestore/ui/main/MainViewModel.kt
@@ -1,28 +1,17 @@
 package com.example.acmeonlinestore.ui.main
 
 import android.app.Application
-import android.content.res.Resources
-import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.example.acmeonlinestore.R
+import com.example.acmeonlinestore.extensions.readAsString
 import com.example.acmeonlinestore.models.Product
 import com.example.acmeonlinestore.models.Products
 import kotlinx.coroutines.launch
-import kotlinx.serialization.Serializable
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
-import java.io.BufferedReader
-import java.io.InputStream
-import java.io.InputStreamReader
-import java.io.Reader
-import java.io.StringWriter
-import java.io.Writer
-
-@Serializable
-data class Foo(val name: String)
 
 class MainViewModel(application: Application) : AndroidViewModel(application) {
 
@@ -37,35 +26,11 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
   }
 
   private fun loadProducts(): Products {
-    val json = loadProductsJson(getApplication<Application>().resources)
+    val json = getApplication<Application>().resources
+      .openRawResource(R.raw.products)
+      .readAsString()
+      .orEmpty()
     return Json { ignoreUnknownKeys = true }.decodeFromString<Products>(json)
-  }
-
-  private fun loadProductsJson(resources: Resources): String {
-    return loadRawJson(resources, R.raw.products)
-  }
-
-  private fun loadRawJson(resources: Resources, id: Int): String {
-    val stream: InputStream = resources.openRawResource(id)
-    val writer: Writer = StringWriter()
-    val buffer = CharArray(1024)
-    try {
-      val reader: Reader = BufferedReader(InputStreamReader(stream, "UTF-8"))
-      var n: Int
-      while (reader.read(buffer).also { n = it } != -1) {
-        writer.write(buffer, 0, n)
-      }
-    } catch (t: Throwable) {
-      Log.e("***", "loadRawJson: error!", t)
-    }
-
-    try {
-      stream.close()
-    } catch (t: Throwable) {
-      Log.e("***", "loadRawJson: error closing input stream", t)
-    }
-
-    return writer.toString()
   }
 }
 
